ca7caf2646abf143830dd7b157434eb6593b0921
Rof1yev <rofiyevdilshod@gmail.com>
2023-09-10 10:13:44 +0500

    tailwind css init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home(): JSX.Element {
   return (
     <main>
       <h1>Hello World!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </main>
   );
 }
